65e459329328dc2fb86abe8cd68d9bf205aab7fe
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-11-05 21:43:57 -0500

    add login view and template

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, LoginView
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name="dashboard"),
+    path('login/', LoginView.as_view(), name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+
+
+class LoginView(LoginView):
+    template_name = "login.html"
+    next_page = "dashboard"
 
 
 class IndexView(TemplateView):
