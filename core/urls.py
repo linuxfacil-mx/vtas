94ab61b2d3ac69a7f72f9b5b624e6f4848089aeb
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-11-05 22:00:28 -0500

    add logout funtionality

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexView, LoginView
+from .views import IndexView, LoginView, logout_view
 
 urlpatterns = [
     path('', IndexView.as_view(), name="dashboard"),
     path('login/', LoginView.as_view(), name="login"),
+    path('logout/', logout_view, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-# from django.shortcuts import render
+from django.shortcuts import redirect
 # from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -8,6 +9,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class LoginView(LoginView):
     template_name = "login.html"
     next_page = "dashboard"
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
