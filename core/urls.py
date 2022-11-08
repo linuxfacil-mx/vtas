20c8009d60622f88cc9296d1a1049c2eada57b9f
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-11-08 13:52:59 -0600

    add CreateView to Store Views

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import IndexView, LoginView, logout_view, StoreIndexView
+from .views import IndexView, CustomLoginView, logout_view, StoreIndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='dashboard'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path('stores/', StoreIndexView.as_view(), name='stores'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
+from django.urls import reverse_lazy
 from django.shortcuts import redirect
 # from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -8,7 +10,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from core.models import Store
 
 
-class LoginView(LoginView):
+class CustomLoginView(LoginView):
     template_name = "login.html"
     next_page = "dashboard"
 
@@ -23,9 +25,11 @@ class IndexView(LoginRequiredMixin, TemplateView):
     extra_context = {'title': 'Dashboard', 'dashboard_active': True}
 
 
-class StoreIndexView(ListView):
+class StoreIndexView(CreateView, ListView):
     template_name = 'core/store-index.html'
     model = Store
+    fields = ['code', 'name', 'address', 'phone']
+    success_url = reverse_lazy('stores')
     extra_context = {'title': 'Tiendas',
                      'admin_active': True,
                      'stores_active': True}
