321a908e532894cea5cdad295549497fc28c1cb6
Jorge Ramirez <jorge_ramirez@outlook.com>
2023-04-07 14:55:16 -0500

    Create status model

--- purchases/admin.py
+++ purchases/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from purchases.models import POHeader, PODetail
+from purchases.models import POHeader, PODetail, POStatus
 
 admin.site.register(POHeader)
 admin.site.register(PODetail)
+admin.site.register(POStatus)
